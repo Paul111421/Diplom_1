c51afeb756fab9db112bc42461632a0618d8e75f
Paul111421 <paulgelfenshtei3@mail.ru>
2026-04-30 17:45:30 +0300

    added mock calls for list, burger is now real (there must be an old joke)

--- src/test/java/com/tests/burger/BurgerBaseTest.java
+++ src/test/java/com/tests/burger/BurgerBaseTest.java
@@ -1,23 +1,33 @@
 package com.tests.burger;
 
-import org.mockito.Mock;
+import org.junit.Before;
 import praktikum.Bun;
 import praktikum.Burger;
-import praktikum.Database;
 import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 import java.util.List;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class BurgerBaseTest {
 
-    @Mock
     Burger burgerForTest = new Burger();
+    List<Bun> bunsForTest = mock(List.class);
+    List<Ingredient> ingredientsForTest = mock(List.class);
 
-    @Mock
-    List<Bun> bunsForTest = new Database().availableBuns();
+    //Выглядит громоздко, но если совсем разрывать зависимость - пусть так.
+    @Before
+    public void setUp(){
 
-    @Mock
-    List<Ingredient> ingredientsForTest = new Database().availableIngredients();
+        when(bunsForTest.get(0)).thenReturn(new Bun("black bun", 100));
 
+        //Одно неудобно - с типом ингредиентов разрыв не выйдет, иначе объект класса не вернётся...
+        when(ingredientsForTest.get(1)).thenReturn(new Ingredient(IngredientType.SAUCE, "sour cream", 200));
+        when(ingredientsForTest.get(2)).thenReturn(new Ingredient(IngredientType.SAUCE, "chili sauce", 300));
+        when(ingredientsForTest.get(3)).thenReturn(new Ingredient(IngredientType.FILLING, "cutlet", 100));
+
+    }
 }
 
